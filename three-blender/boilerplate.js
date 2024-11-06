d4ac470c816b6289f6703ee938a00b3b325c09ac
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-11-06 23:06:35 +0530

    init

---
+++ three-blender/boilerplate.js
@@ -0,0 +1,2 @@
+import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
+const scene = new THREE.Scene();
---
+++ three-blender/main.py
@@ -0,0 +1,47 @@
+import bpy
+
+
+# CAMERAS
+
+cam_code = ""
+
+for i in bpy.data.cameras:
+    print("\n")
+
+    cam_loc = bpy.data.objects[i.name].location
+    cam_rot = bpy.data.objects[i.name].rotation_euler
+   
+    print("CAMERA NAME: ", i.name)
+    print("CAMERA LOCATION: ", cam_loc)
+    print("CAMERA ROTATION: ", cam_rot)
+    print("CAMERA TYPE: ", i.type)
+    print("CAMERA LENS: ", i.lens)
+    print("CAMERA SENSOR WIDTH: ", i.sensor_width)
+    print("CAMERA SENSOR HEIGHT: ", i.sensor_height)
+    print("CAMERA SENSOR FIT: ", i.sensor_fit)
+
+    cam_code += f"const {i.name} = new THREE.PerspectiveCamera({i.lens}, window.innerWidth / window.innerHeight, 0.1, 1000);\n"
+    cam_code += f"{i.name}.position.set({cam_loc.x}, {cam_loc.y}, {cam_loc.z});\n"
+    cam_code += f"{i.name}.rotation.set({cam_rot.x}, {cam_rot.y}, {cam_rot.z});\n"
+
+    print("\n")
+    print(cam_code)
+
+# LIGHTS
+
+light_code = ""
+
+for i in bpy.data.lights:
+
+    print("\n")
+
+    light_loc = bpy.data.objects[i.name].location
+    light_rot = bpy.data.objects[i.name].rotation_euler
+
+    print("LIGHT NAME: ", i.name)
+    print("LIGHT LOCATION: ", light_loc)
+    print("LIGHT ROTATION: ", light_rot)
+    print("LIGHT TYPE: ", i.type)
+    print("LIGHT COLOR: ", i.color)
+    print("LIGHT ENERGY: ", i.energy)
+    print("LIGHT POWER: ", i.power)
